17227883a6870769a9e7bee4ef43a11966f1a671
Titus Brandy <tbrands13@gmail.com>
2021-08-10 18:37:58 -0500

    INIT COMMIT

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomPasscode.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("Passcode", createPass());
+            ViewBag.Password = HttpContext.Session.GetString("Passcode");
+            Console.WriteLine(ViewBag.Password);
+            return View();
+        }
+        private string createPass()
+        {
+            string [] code = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+            Random rand = new Random();
+            string results = "";
+            for(int i = 0; i < 14; i++ )
+                results += code[rand.Next(code.Length)];
+            return results;
+            
+        }
+        [HttpPost("submit")]
+        public IActionResult Submit(string passcode)
+        {
+            // HttpContext.Session.SetString("Passcode", createPass());
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
